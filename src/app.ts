ee1d5d1a4e77e579dc9d38e7d0f98fe84fdaf4fe
Wylham <wylhamdev@gmail.com>
2026-08-24 22:39:35 -0300

    feat: inicio de implementacao de backend app.ts , server.ts part 2

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,9 @@ import express from "express";
 const app = express();
 
 export default app;
+
+app.get("/health", (req, res) => {
+  res.send("Api Rodando Tranquilamente");
+});
+
+app.listen(3000);
