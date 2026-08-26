a24e887c5049a27720762947bf02dd81c4724683
Wylham <wylhamdev@gmail.com>
2026-08-25 23:43:09 -0300

    feat: criacao dos primeiros endpoints do app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
 import express from "express";
+import { type Request, type Response, type NextFunction } from "express";
 
 const app = express();
 
-export default app;
-
-app.get("/health", (req, res) => {
-  res.send("Api Rodando Tranquilamente");
+app.get("/", (req, res) => {
+  res.send("Api Rodando Tranquilamente...");
 });
 
-app.listen(3000);
+app.get("/clan", (req, res) => {
+  res.json({
+    message: "Caminho para os clãs futuramente",
+    status: "ok",
+  });
+});
+
+app.get("/player", (req, res) => {
+  res.json({
+    message: "Caminho para os players futuramente",
+    status: "ok",
+  });
+});
+
+app.use(express.json());
+
+export default app;
