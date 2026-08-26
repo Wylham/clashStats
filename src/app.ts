8d03a8e357c30d3447f63a67abb4421af7401524
Wylham <wylhamdev@gmail.com>
2026-08-26 00:05:18 -0300

    feat: ajuste de clean code para a funcao express.json

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { type Request, type Response, type NextFunction } from "express";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Api Rodando Tranquilamente...");
@@ -18,7 +19,5 @@ app.get("/player", (req, res) => {
     status: "ok",
   });
 });
-
-app.use(express.json());
 
 export default app;
